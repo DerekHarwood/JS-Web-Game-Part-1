ee28fa69dc5cc3814f344df5ab32d79ec5476caf
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-06-08 17:18:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
-    return object
+let temp = 60  
+let desiredTemp = 72
+
+while (temp < desiredTemp) {
+    temp +=1
 }
 
-function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
-
-    object.addEventListener('dblclick', () => {
-        object.remove()
-    })
-}
-
-
-
-
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
-
-newItem('assets/sword.png', 500, 405)
-newItem('assets/sheild.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+console.log("The temperature is now" + temp+"F!")
